b7b25a80f3dda43b876323fb3a6cd66bbd7b1a01
pritom-dey1 <Pritom1.2.zx@gmail.com>
2025-09-11 15:21:04 +0600

    Fix allowed hosts for Render

--- blood_donor/settings.py
+++ blood_donor/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-)wddrh^se1i4g-0#d@_gdu5l%6)!o6_d*k$@e&2kpx^peoq)$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["zero-boundaries-hackathon.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 # Application definition
